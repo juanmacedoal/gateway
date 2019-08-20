1edacc74df53799960881d13a9951b40c2193855
jmmacedo <jmmacedo@ITEM-S84820.emea.msad.sopra>
2019-08-20 12:32:33 +0200

    repair

--- src/main/java/com/gateway/configuration/CorsConfiguration.java
+++ src/main/java/com/gateway/configuration/CorsConfiguration.java
@@ -4,38 +4,35 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;import
- org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-/****Application configuration.****
+/****
+ * Application configuration.****
  *
  */
- @Configuration
- @EnableZuulProxy
- public class CorsConfiguration extends WebMvcConfigurerAdapter
- {
+@Configuration
+@EnableZuulProxy
+public class CorsConfiguration extends WebMvcConfigurerAdapter {
 
- /**
- * CORS configuration.
- */
- @Bean
- public CorsFilter corsFilter()
- {
- final UrlBasedCorsConfigurationSource source = new
- UrlBasedCorsConfigurationSource();
- final org.springframework.web.cors.CorsConfiguration config = new
- org.springframework.web.cors.CorsConfiguration();
- config.setAllowCredentials(true);
- config.addAllowedOrigin("*");
- config.addAllowedHeader("*");
- config.addAllowedMethod("OPTIONS");
- config.addAllowedMethod("HEAD");
- config.addAllowedMethod("GET");
- config.addAllowedMethod("PUT");
- config.addAllowedMethod("POST");
- config.addAllowedMethod("DELETE");
- config.addAllowedMethod("PATCH");
- source.registerCorsConfiguration("/**", config);
- return new CorsFilter(source);
- }
- }
+	/**
+	 * CORS configuration.
+	 */
+	@Bean
+	public CorsFilter corsFilter() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		final org.springframework.web.cors.CorsConfiguration config = new org.springframework.web.cors.CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("OPTIONS");
+		config.addAllowedMethod("HEAD");
+		config.addAllowedMethod("GET");
+		config.addAllowedMethod("PUT");
+		config.addAllowedMethod("POST");
+		config.addAllowedMethod("DELETE");
+		config.addAllowedMethod("PATCH");
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
+}
